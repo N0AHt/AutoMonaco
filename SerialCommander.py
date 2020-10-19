81f5272cb84120446084a59e32816945031667b9
Noah Telerman <n.telerman@gmail.com>
2020-10-19 18:24:51 +0100

    Rough serial control class

--- SerialCommander.py
+++ SerialCommander.py
@@ -2,6 +2,31 @@
 #19/10/2020
 
 #(abstract) Class to control simple data flow via serial ports
+# wrapper for pyserial to make things easy
 
-class SerialCommander():
+import serial
+
+class SerialCommander:
+
+    def __init__(self,Port_id, baudrate):
+        self.port_id = Port_id
+        self.baudrate = baudrate
+        self.port = serial.Serial(port_id)
+
+    def openPort():
+        (self.port).open()
+
+    def closePort():
+        (self.port).close()
+
+    def readline():
+        (self.port).readline()
+
+    def write(string_input):
+        (self.port).write(str(string_input))
+
+    def portID():
+        self.port
+        print('\n Port Open: ', (self.port).is_open)
+
     

40f9ccafc3d9b98c170bf222e8e286e42ef2856f
Noah Telerman <n.telerman@gmail.com>
2020-10-27 12:03:23 +0000

    start up function added
    4-22 from coherent manual

--- Monaco.py
+++ Monaco.py
@@ -13,6 +13,7 @@ class Monaco(SerialCommander):
 
         self.power = power
         self.pulse_freq = pulse_freq
+        self.shutter_position = None
 
         #check if port is open
         if self.check_open() = False:
@@ -29,31 +30,43 @@ class Monaco(SerialCommander):
         laser_temp = self.query('?BT')
         print('Laser temperature = ', laser_temp)
 
-    #
-    # def power_on(self):
-    #     # protocol to power on the laser safely
-    #     # will need many safety checks here
-    #
-    #     #Activate chillers
-    #     self.write('CHEN=1')
-    #     #Note time of activation
-    #
-    #     #check keyswitch
-    #     self.write('?k')
-    #     if self.readline() != 1:
-    #         print('\n keyswitch off \n')
-    #         keycheck = input('turn keyswitch on [y/n]')
-    #
-    #     #Wait Cycle (3 mins?)
-    #
-    #     #Manual check
-    #     laserCheck = 'n'
-    #     while laserCheck != 'y':
-    #         laserCheck = input('\n !Confirm Laser Ready! [y/n] \n')
-    #
-    #     #turn on laser
-    #     self.write()
-    #
+
+    def power_on(self):
+        # protocol to power on the laser safely
+        # will need many safety checks here
+
+        #Step1 - Chillers on
+        self.serial_write('CHEN=1')
+
+        #Wait Cycle (3 mins?)
+        time.sleep(180)
+
+        #check keyswitch
+        key_status = self.query('?k')
+        if key_status != 1:
+            print('\n keyswitch off \n')
+            keycheck = input('turn keyswitch on [y/n]')
+
+        #check for faults
+        faults_status = self.query('?F')
+        print(faults_status)
+        #check for warnings
+        warning_status = self.query('?W')
+        print(warning_status)
+
+        #Close Shutters
+        self.serial_write('S=0')
+        self.shutter_position = self.query('?S')
+        print('self.shutter_position')
+
+        #Manual check
+        laserCheck = 'n'
+        while laserCheck != 'y':
+            laserCheck = input('\n !Confirm Laser Ready! [y/n] \n')
+
+        #turn on laser
+        self.write()
+
     # def power_off(self):
     #     #protocol for turning off the laser
     #     #probably will need safety protocols here too

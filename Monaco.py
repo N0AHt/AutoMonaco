04df90abb0a984f394be7f2ce2253a2068c716bd
Noah Telerman <n.telerman@gmail.com>
2020-10-27 12:33:05 +0000

    Activate laser function

--- Monaco.py
+++ Monaco.py
@@ -14,9 +14,10 @@ class Monaco(SerialCommander):
         self.power = power
         self.pulse_freq = pulse_freq
         self.shutter_position = None
+        self.laser_ready = False
 
         #check if port is open
-        if self.check_open() = False:
+        if self.check_open() == False:
             print('Opening Port \n')
             self.openPort()
         else:
@@ -31,8 +32,8 @@ class Monaco(SerialCommander):
         print('Laser temperature = ', laser_temp)
 
 
-    def power_on(self):
-        # protocol to power on the laser safely
+    def start_up(self):
+        # protocol to power on the laser safely - doesnt turn on diodes
         # will need many safety checks here
 
         #Step1 - Chillers on
@@ -63,9 +64,37 @@ class Monaco(SerialCommander):
         laserCheck = 'n'
         while laserCheck != 'y':
             laserCheck = input('\n !Confirm Laser Ready! [y/n] \n')
+            self.laser_ready = True
 
-        #turn on laser
-        self.write()
+#needs to be run AFTER the setup function, should include checks or include in
+#the same function as start_up()
+    def activate_laser(self, pulse_mode):
+        if self.laser_ready == True:
+            #set pulse mode
+            self.serial_write('PM=',str(pulsemode))
+            self.query('?PM')
+
+            #Turn on diodes
+            if self.query('?S') == 0:
+                self.serial_write('L=1')
+            elif self.query('?S') != 0:
+                self.serial_write('S=0')
+                self.serial_write('L=1')
+            print('\n Warming Diodes \n')
+
+            #wait for diodes to warm
+            diode_ready = 'OFF'
+            while diode_ready != 'On':
+                diode_ready = self.query('?ST')
+                print('*', end = '')
+                time.sleep(10)
+            print('\n')
+
+        else:
+            print('LASER NOT READY - run start_up step')
+
+    def start_lasing(self):
+        
 
     # def power_off(self):
     #     #protocol for turning off the laser

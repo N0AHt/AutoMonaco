7288da199c0087d2166e498d52cce8f3d8f27add
Noah Telerman <n.telerman@gmail.com>
2020-11-20 16:01:06 +0000

    testing checks

--- testScript.py
+++ testScript.py
@@ -5,6 +5,9 @@ port = 'com1'
 laser = Monaco(Port_id = port, power = 10, pulse_freq = 10)
 RL_values = [10,20,30,40,50,60,70,80,90,100]
 
+print('diode ready: ', laser.diode_ready)
+print('laser ready: ', laser.laser_ready)
+
 for RL in RL_values:
     laser.start_lasing()
     time.sleep(10)

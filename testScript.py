cd7490944bbbe94e4ece509e438a107dc3082887
Noah Telerman <n.telerman@gmail.com>
2020-11-20 15:57:57 +0000

    syntax fix

--- testScript.py
+++ testScript.py
@@ -5,7 +5,7 @@ port = 'com1'
 laser = Monaco(Port_id = port, power = 10, pulse_freq = 10)
 RL_values = [10,20,30,40,50,60,70,80,90,100]
 
-for RL in Rl_values:
+for RL in RL_values:
     laser.start_lasing()
     time.sleep(10)
     laser.stop_lasing()

9df9c45be6ed18394c8459e42968057cf5634b63
Noah Telerman <n.telerman@gmail.com>
2020-11-20 14:31:32 +0000

    more syntax

--- Monaco.py
+++ Monaco.py
@@ -50,7 +50,7 @@ class Monaco(SerialCommander):
             self.laser_ready = False
 
         #Diode Ready check - ready to open shutters and fire laser
-        if self.diode_status = 1 and self.pulse_status == 1:
+        if self.diode_status == 1 and self.pulse_status == 1:
             self.diode_ready = True
         else:
             self.diode_ready = False

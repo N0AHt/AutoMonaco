b91a4aba1ab0eb6a749c6fddaa582684c55233fd
Noah Telerman <n.telerman@gmail.com>
2020-12-07 14:11:47 +0000

    skeleton powermeter class

---
+++ PowerMeter.py
@@ -0,0 +1,15 @@
+#Code to control thorlabs powermeter remotely
+#Noah telerman 2020
+
+class PowerMeter():
+    def __init__(wavelength):
+
+        #set wavelength of power to record
+        self.wavelength = wavelength
+
+    def ReadPower():
+
+        return power_output
+
+    def SetWavelength():
+        

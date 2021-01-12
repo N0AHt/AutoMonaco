cda34268620179749dd5df2883714eade8d7dd01
Noah Telerman <n.telerman@gmail.com>
2021-01-12 16:06:01 +0000

    Fixed EOF stuff

--- SerialCommander.py
+++ SerialCommander.py
@@ -51,7 +51,6 @@ class SerialCommander:
 
     def query(self, string_input):
         response = self.serial_write(string_input)
-        print('1: ', response)
         response = self.remove_EOF(response)
         return response
 

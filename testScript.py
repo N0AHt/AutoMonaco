bf1d5656293641acbd8274967323c044f02e926f
Noah Telerman <n.telerman@gmail.com>
2021-01-12 13:11:55 +0000

    fixed commented line

--- testScript.py
+++ testScript.py
@@ -9,8 +9,8 @@ RL_values = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100]
 
 for RL in RL_values:
 
-    #laser.status_report()
-    #laser.set_parameters(power = RL)
+    laser.status_report()
+    laser.set_parameters(power = RL)
     print(laser.power)
     laser.start_lasing()
     time.sleep(10)

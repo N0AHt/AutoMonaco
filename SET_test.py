c1424480e98b21192d5e72b1b99bb9dc7fbe6c08
Noah Telerman <n.telerman@gmail.com>
2021-01-19 15:11:51 +0000

    changing rep rate value

--- SET_test.py
+++ SET_test.py
@@ -5,7 +5,7 @@ port = 'com1'
 laser = Monaco(Port_id = port, power = 10, pulse_freq = 200)
 
 #values in kHz
-RepRate_values = [200, 250, 330, 500, 1000, 2000, 4000, 10000, 50000]
+RepRate_values = [400, 250, 330, 500, 1000, 2000, 4000, 10000, 50000]
 
 
 for MRR in RepRate_values:

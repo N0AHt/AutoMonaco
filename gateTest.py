22e92c6bffb901e860279a30a143b0aa69b29b85
Noah Telerman <n.telerman@gmail.com>
2021-03-05 15:54:25 +0000

    added sleep

--- gateTest.py
+++ gateTest.py
@@ -13,6 +13,7 @@ gate = ArduinoGate(port = port_id, baudrate = 9600, timeout = 5)
 for i in range(5):
     delay = input('delay: ')
     gate.quick_open(int(delay))
+    time.sleep(1)
     print('output: ', gate.serial_read())
     #time.sleep(3)
 

5065eafd5b65e96684ab6a643aadd5efed48824b
Noah Telerman <n.telerman@gmail.com>
2021-03-11 15:12:05 +0000

    added delay and reading from gate

--- gateTest2.py
+++ gateTest2.py
@@ -3,7 +3,7 @@ from Monaco import Monaco
 import time
 
 laser = Monaco(Port_id = port, power = 80, pulse_freq = 1000)
-gate = ArduinoGate(port = port = 'com4', baudrate = 9600, timeout = 5)
+gate = ArduinoGate(port ='com4', baudrate = 9600, timeout = 5)
 
 laser.activate_laser(1) #gated Mode
 
@@ -12,5 +12,7 @@ laser.start_lasing()
 for i in range(5):
     open_time = input('Delay: ')
     gate.quick_open(open_time)
+    time.sleep(2)
+    print('output: ', gate.serial_read())
 
 laser.stop_lasing()

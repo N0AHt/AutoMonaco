dfcbb8bfb7f7f09642d9c7f180fcef67a1264eae
Noah Telerman <n.telerman@gmail.com>
2021-03-15 15:53:57 +0000

    syntax

--- burstTest.py
+++ burstTest.py
@@ -7,15 +7,17 @@ gate = ArduinoGate(port ='com4', baudrate = 9600, timeout = 5)
 
 laser.set_parameters(RRD = 1)
 laser.status_report()
-laser.stop_lasing()
+
 laser.start_lasing()
 
 time.sleep(5)
 
-laser.stop_lasing()
+
 
 print('opening gate')
 
 gate.quick_open(5000)
 
 time.sleep(10)
+
+laser.stop_lasing()

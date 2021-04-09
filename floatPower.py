fdebf8b0e95e08cdb516dfdc990e5597bb8c567b
Noah Telerman <n.telerman@gmail.com>
2021-04-09 14:55:17 +0100

    another power

--- floatPower.py
+++ floatPower.py
@@ -3,7 +3,7 @@ import time
 
 port = 'com1'
 laser = Monaco(Port_id = port, power = 10, pulse_freq = 1000)
-error = laser.serial_write('RL=50.505')
+error = laser.serial_write('RL=52.713')
 
 print(error)
 

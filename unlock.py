e4bdd98b9e432e80ee59747e663a92803253529c
Noah Telerman <n.telerman@gmail.com>
2021-06-23 16:10:03 +0100

    updated unlock script

--- unlock.py
+++ unlock.py
@@ -7,6 +7,6 @@ print('Port list: ', Monaco.port_finder())
 port = input('input port: ')
 
 #Open port and check connection is secure
-laser = Monaco(Port_id = port, power = 10, pulse_freq = 10)
-laser.serial_write('LOCKOUT=0')
+laser = Monaco(Port_id = port)
+laser.laser_unlock()
 print('unlocked laser port')
